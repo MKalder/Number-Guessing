42f09df63e921c2b8446b81524f60b1bddbb33cd
Marius Kalder <147047923+MKalder@users.noreply.github.com>
2026-05-02 20:00:15 +0700

    adjusted script

--- assets/js/play.js
+++ assets/js/play.js
@@ -1,5 +1,6 @@
 // var initialization
-const button = document.getElementById("play-btn");
+const buttonPlay = document.getElementById("play-btn");
+const buttonRestart = document.getElementById("restart-btn");
 
 const gameTitle = document.querySelector(".game-title");
 const infoGame = document.querySelector(".game-subline");
@@ -8,14 +9,18 @@ infoGame.textContent = "Enter a number in the range of 1 - 20 and hit the play b
 // game specific numbers
 let playedGames = 0;
 let highscore = 0;
-let attempts = 20;
+let attempts = 5;
 const random = randomNumber();
 const inputNumber = document.getElementById
     ('input-number');
 const attemptsCounter = document.querySelector(".score>span");
-attemptsCounter.textContent = attempts;
+const highscoreNumber = document.querySelector(".highscore>span");
 
-button.addEventListener("click", play);
+attemptsCounter.textContent = attempts;
+highscoreNumber.textContent = highscore;
+
+buttonPlay.addEventListener("click", play);
+buttonRestart.addEventListener("click", newGame);
 
 
 // functions
@@ -27,23 +32,36 @@ function randomNumber() {
 function play() {
     attempts--;
 
-
     if (random === Number(inputNumber.value)) {
-        gameTitle.textContent = "Congratulations 🥳"
+        gameTitle.textContent = "Congratulations 🥳";
+        infoGame.textContent = "JA 🏆";
+        buttonPlay.disabled = true;
         if (highscore < attempts) {
             highscore = attempts;
+            highscoreNumber.textContent = highscore;
         }
         playedGames++;
-
     }
+
+    // TODO: Bug issue
+
     if (attempts === 0) {
         gameTitle.textContent = "...GAME OVER 😔";
         infoGame.textContent = "No more attempts left";
+        buttonPlay.disabled = true;
+    } else if (random > Number(inputNumber.value)) {
+        infoGame.textContent = "Input is smaller than my number."
+    } else {
+        infoGame.textContent = "Input is greater than my number."
     }
 
     attemptsCounter.textContent = attempts;
 }
 
 function newGame() {
-    // TODO
+    buttonPlay.disabled = false;
+    attempts = 5;
+    gameTitle.textContent = "Another Round 😀";
+    infoGame.textContent = "Game Number " + playedGames;
+
 }
