1a54a79cf32a4abbdce172a76c7ca15949a19434
Marius Kalder <147047923+MKalder@users.noreply.github.com>
2026-05-02 21:48:50 +0700

    some minor fixes and ui experiments

--- assets/js/play.js
+++ assets/js/play.js
@@ -1,24 +1,27 @@
 // var initialization
 const buttonPlay = document.getElementById("play-btn");
 const buttonRestart = document.getElementById("restart-btn");
-
 const gameTitle = document.querySelector(".game-title");
 const infoGame = document.querySelector(".game-subline");
-infoGame.textContent = "Enter a number in the range of 1 - 20 and hit the play button! How fast can you guess my number?";
 
 // game specific numbers
 let playedGames = 0;
 let highscore = 0;
 let attempts = 5;
-const random = randomNumber();
+let random = randomNumber();
+
 const inputNumber = document.getElementById
     ('input-number');
+
 const attemptsCounter = document.querySelector(".score>span");
 const highscoreNumber = document.querySelector(".highscore>span");
 
+// text changes
+infoGame.textContent = "Enter a number in the range of 1 - 20 and hit the play button! How fast can you guess my number?";
 attemptsCounter.textContent = attempts;
 highscoreNumber.textContent = highscore;
 
+// listeners
 buttonPlay.addEventListener("click", play);
 buttonRestart.addEventListener("click", newGame);
 
@@ -29,39 +32,52 @@ function randomNumber() {
     return ran;
 }
 
+function checkGuess(guess) {
+    if (!guess || guess < 1 || guess > 20) {
+        infoGame.textContent = "Please enter a number between 1 and 20.";
+        return false;
+    }
+    return true;
+}
+
+//updateUI()
+//endGame()
+//resetGame()
+
 function play() {
-    attempts--;
+    const guess = Number(inputNumber.value);
+    console.log(guess);
 
-    if (random === Number(inputNumber.value)) {
-        gameTitle.textContent = "Congratulations 🥳";
-        infoGame.textContent = "JA 🏆";
-        buttonPlay.disabled = true;
-        if (highscore < attempts) {
-            highscore = attempts;
-            highscoreNumber.textContent = highscore;
+    if (checkGuess(guess)) {
+        attempts--;
+        if (random === guess) {
+            gameTitle.textContent = "Congratulations 🥳";
+            infoGame.textContent = "JA 🏆";
+            buttonPlay.disabled = true;
+            if (highscore < attempts) {
+                highscore = attempts;
+                highscoreNumber.textContent = highscore;
+            }
+        } else if (attempts === 0) {
+            gameTitle.textContent = "...GAME OVER 😔";
+            infoGame.textContent = "No more attempts left";
+            buttonPlay.disabled = true;
+        } else if (random > guess) {
+            infoGame.textContent = "Input is smaller than my number."
+        } else {
+            infoGame.textContent = "Input is greater than my number."
         }
-        playedGames++;
+        attemptsCounter.textContent = attempts;
     }
 
-    // TODO: Bug issue
-
-    if (attempts === 0) {
-        gameTitle.textContent = "...GAME OVER 😔";
-        infoGame.textContent = "No more attempts left";
-        buttonPlay.disabled = true;
-    } else if (random > Number(inputNumber.value)) {
-        infoGame.textContent = "Input is smaller than my number."
-    } else {
-        infoGame.textContent = "Input is greater than my number."
-    }
-
-    attemptsCounter.textContent = attempts;
 }
 
 function newGame() {
+    playedGames++;
     buttonPlay.disabled = false;
     attempts = 5;
+    attemptsCounter.textContent = attempts;
+    random = randomNumber();
     gameTitle.textContent = "Another Round 😀";
-    infoGame.textContent = "Game Number " + playedGames;
-
+    infoGame.textContent = "Game Number " + playedGames + " Enter a number in the range of 1 - 20 and hit the play button! How fast can you guess my number?";
 }
