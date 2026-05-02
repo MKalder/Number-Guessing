87b7dbc143ed6afadd4b8d0829439a8115664e36
Marius Kalder <147047923+MKalder@users.noreply.github.com>
2026-05-02 17:01:34 +0700

    adding js functionality

--- assets/js/play.js
+++ assets/js/play.js
@@ -1,1 +1,49 @@
-const inputNumber = 
+// var initialization
+const button = document.getElementById("play-btn");
+
+const gameTitle = document.querySelector(".game-title");
+const infoGame = document.querySelector(".game-subline");
+infoGame.textContent = "Enter a number in the range of 1 - 20 and hit the play button! How fast can you guess my number?";
+
+// game specific numbers
+let playedGames = 0;
+let highscore = 0;
+let attempts = 20;
+const random = randomNumber();
+const inputNumber = document.getElementById
+    ('input-number');
+const attemptsCounter = document.querySelector(".score>span");
+attemptsCounter.textContent = attempts;
+
+button.addEventListener("click", play);
+
+
+// functions
+function randomNumber() {
+    const ran = Math.floor(Math.random() * 20 + 1);
+    return ran;
+}
+
+function play() {
+    attempts--;
+
+
+    if (random === Number(inputNumber.value)) {
+        gameTitle.textContent = "Congratulations 🥳"
+        if (highscore < attempts) {
+            highscore = attempts;
+        }
+        playedGames++;
+
+    }
+    if (attempts === 0) {
+        gameTitle.textContent = "...GAME OVER 😔";
+        infoGame.textContent = "No more attempts left";
+    }
+
+    attemptsCounter.textContent = attempts;
+}
+
+function newGame() {
+    // TODO
+}
--- assets/js/random.js
+++
@@ -1,8 +0,0 @@
-function randomNumber() {
-    const ran = Math.floor(Math.random() * 20 + 1);
-    console.log(ran);
-    return ran;
-}
-
-
-
